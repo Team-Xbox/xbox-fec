4950874c7b08bd4b68a451859370be281faccdc3
Yari Torres Nicola <ybtorres9@gmail.com>
2022-08-24 09:44:20 -0600

    creating master repo

---
+++ public/bundle.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'react-dom/client'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\nvar root = Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'react-dom/client'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(document.getElementById(\"root\")); // Huzzah for jsx!\n\nvar App = function App() {\n  return /*#__PURE__*/React.createElement(\"h1\", null, \"Hello World\");\n};\n\nroot.render( /*#__PURE__*/React.createElement(App, null));\n\n//# sourceURL=webpack://xbox-fec/./src/index.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
