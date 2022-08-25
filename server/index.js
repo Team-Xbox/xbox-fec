63ca84aeeb13ae7714630bc13b9b67b8571ec5e6
Yari Torres Nicola <ybtorres9@gmail.com>
2022-08-25 16:33:18 -0600

    added route to express server named /styles as well as created new Slideshow component. also installed cors for use with express

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const api = require('./api.js');
+const cors = require('cors');
 
 app.use(express.json());
-app.use(express.static(path.join('public')));
+app.use(cors());
+app.use('src', express.static(path.join(__dirname, 'ProductDetail', 'components')));
+
+app.get('/styles', (req, res) => {
+  api.getStyleData(66642)
+  .then(response => { res.send(response.data) })
+  .catch(err => console.log(err))
+})
 
 app.listen(1337, () => {
   console.log('Listening on port 1337');
