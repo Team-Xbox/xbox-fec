fa7eab98f1325a53f4207e4ca4155be4911efc69
Adam Blomberg <adamablomberg@gmail.com>
2022-08-30 15:40:19 -0500

    This is a commit with organized test folder and location for tests.

---
+++ tests/app.test.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import '@testing-library/react/dont-cleanup-after-each';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import {render, screen, waitFor} from '@testing-library/react';
+import App from '../src/app.jsx';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000';
+
+//Yari
+describe('', function() {
+
+});
+
+//Selina
+describe('', function() {
+
+});
+
+//Adam
+describe('', function() {
+
+});
