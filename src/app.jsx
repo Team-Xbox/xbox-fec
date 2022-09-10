a75705ec34d7afd9f3067f6f95ec2cfdd6ec0670
selinaduran <selinad177@gmail.com>
2022-09-10 08:47:39 -0500

    saving app.jsx file

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ProductDetail from "./ProductDetail/ProductDetail.jsx";
 import QuestionsList from "./Q&A/QuestionsList.jsx";
+import RelatedProductsList from "./RelatedItems/RelatedProductsList.jsx";
 import RatingsAndReviews from "./RatingsAndReviews/RatingsAndReviews.jsx";
-import Header from './Header/Header.jsx'
 
 // Huzzah for jsx!
 const App = () => {
   return (
     <div>
-      <Header />
-      <div className="announcement">
-        <p> red was seen coming from electrical -- report any sus crewmates -- sale on spacesuits this month only </p>
-      </div>
+      <h1>Hello World</h1>
       <ProductDetail />
+      <RelatedProductsList />
       <QuestionsList />
       <RatingsAndReviews/>
     </div>
